8e1bbfa79233d361b2be1fd3fd59b7a3a9edfa65
Michael Fleig <michifleig@gmail.com>
2020-06-01 15:51:55 +0200

    update

--- DistributionPackages/Fleigm.Site/Resources/Public/Frontend/design/tailwind.config.js
+++ DistributionPackages/Fleigm.Site/Resources/Public/Frontend/design/tailwind.config.js
@@ -1,5 +1,42 @@
 module.exports = {
-	theme: {},
-	variants: {},
+	theme: {
+		fontFamily: {
+			sans: [
+				'"Inter"',
+				'system-ui',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'"Segoe UI"',
+				'"Helvetica Neue"',
+				'Arial',
+				'"Noto Sans"',
+				'sans-serif',
+				'"Apple Color Emoji"',
+				'"Segoe UI Emoji"',
+				'"Segoe UI Symbol"',
+				'"Noto Color Emoji"',
+			],
+		},
+		extend: {
+			colors: {
+			},
+			height: {
+				'96': '24rem',
+			},
+		}
+	},
+	variants: {
+		display: ['responsive', 'group-hover'],
+		scale: ['responsive', 'hover', 'group-hover'],
+		opacity: ['responsive', 'hover', 'group-hover'],
+	},
 	plugins: [],
+
+	corePlugins: {
+		container: false,
+	},
+	purge: [
+		'../../Private/**/*.fusion',
+		'../../Private/**/*.yaml',
+	],
 }
--- DistributionPackages/Fleigm.Site/Resources/Public/Frontend/js/main.js
+++ DistributionPackages/Fleigm.Site/Resources/Public/Frontend/js/main.js
@@ -1,2 +1,20 @@
 import 'alpinejs'
 
+window.dropdown = function () {
+	return {
+		show: false,
+
+		open() {
+			this.show = true;
+		},
+		close() {
+			this.show = false;
+		},
+		toggle() {
+			this.isOpen() ? this.close() : this.open();
+		},
+		isOpen() {
+			return this.show;
+		}
+	}
+}
